4abe9aff39254e0192c749e076182283339674c4
Dmd2007 <dmd2007@wildcats.unh.edu>
2018-02-28 16:13:36 -0500

    start homework1.py

--- hw1.py
+++ hw1.py
@@ -1,16 +1,44 @@
-"""
-Practice with functions 
-lab1run.py
-Dylan Durand
-1/31/2018
+"""This program calculates the area of a circle given the radius"""
+
+radius = float(input('Enter Radius: '))
+PI = 3.14159265359
+area = PI * radius * radius
+
+print("The area of your circle is= %.2f" %area)
+
+
+"""This program calculates the miles per gallon of a car given miles and gallons"""
+
+miles = input("Enter amount of miles: ")
+miles = float(miles)
+ 
+gallons = input("Enter amount of gallons: ")
+gallons = float(gallons)
+ 
+milesper = miles / gallons
+print("You get: ", milesper, "mpg")
+
+
+"""This program calculates the sum of odd numbers given n"""
+
+oddnumbers = int(input("Insert a number to calculate the sum: "))
+total = sum(range(1, oddnumbers+1, 2))
+
+print("The sum of the odd numbers in your range is: ", sum)
+
+"""This program tells whether a number is odd or not"""
+
+def isEven(number):
+   if number!=0:
+   		return(n-2)
+   while number =<2:
+     if number ==2:
+        print("True")
+     else:
+        print("False")
 
 """
 
-import lab1
 
-def run_alarm_clock():
-	"""
-	Test alarm_clock()
-	"""
-	lab1.alarm_clock()
-	
+
+
